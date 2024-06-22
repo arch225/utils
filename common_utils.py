a9ca1e4434e6735de763a15b3b2f6014c5cd0ee1
Rachit Goyal <rachit.promo@gmail.com>
2024-06-22 21:02:10 +0530

    Standardize Code

--- common_utils.py
+++ common_utils.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 
-def list_files(folder_path):
+def F_list_files(folder_path):
     file_list = []
     i=-1
     for root, dirs, files in os.walk(folder_path):
@@ -10,4 +10,15 @@ def list_files(folder_path):
             if ext in ['xlsx', 'csv', 'xls']:
                 i+=1
                 file_list.append([i, root, file, ext])
+#list_files('/Users/dibbi/Desktop/rac_utils')
     return file_list
+
+def F_data_import(loc,ind):
+    x=F_list_files(loc)
+    if x[ind][3]=='xlsx':
+        df = pd.read_excel(x[ind][1]+'/'+x[ind][2])
+    elif x[ind][3]=='csv':
+        df = pd.read_csv(x[ind][1]+'/'+x[ind][2])
+    elif x[ind][3]=='xls':
+        df = pd.read_excel(x[ind][1]+'/'+x[ind][2])
+    return df
